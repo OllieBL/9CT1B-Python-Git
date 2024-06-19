dd7e61c9ac1a6e2890a2365ca0a7cfd6ce73ea4e
OllieBL <oliver.black4@education.nsw.gov.au>
2024-06-20 09:19:16 +1000

    Update adventure_game_main.py

--- python-for-beginners/19 - Checkpoint/adventure_game_main.py
+++ python-for-beginners/19 - Checkpoint/adventure_game_main.py
@@ -8,6 +8,7 @@ import time # import the time module
 gameOver = False # create a variable to control the game loop
 playerHealth = 100 # eate a variable to store the player's health
 playerAttack = 10 # create a variable to store the player's attack power
+x = 0 # I use this for while loops 
 
 #----------------------------------------- SET UP THE GAME BOARD -----------------------------------------
 #create a 5x5 game board. 0 = empty, 1 = player, 2 = enemy, 3 = treasure, 4 = trap, 5 = exit, 6 = boss, 7=visited
@@ -17,15 +18,34 @@ gameBoard = [[0,0,0,0,0],
              [0,0,0,0,0],
              [0,0,0,0,0]]
 
+
+# make the objects non stackable
+def checkStacking(object):
+    global gameBoard
+    objectX = random.randint(0, 4)
+    objectY = random.randint(0, 4)
+    while gameBoard[objectX][objectY] != 0:
+        objectX = random.randint(0, 4)
+        objectY = random.randint(0, 4)
+    gameBoard[objectX][objectY] = object
+
 # place the player in the middle of the game board
 playerX = 2
 playerY = 2
 gameBoard[playerX][playerY] = 1
 
 # place the exit in a random location
-exitX = random.randint(0,4)
-exitY = random.randint(0,4)
-gameBoard[exitX][exitY] = 5
+checkStacking(5)
+
+# place an enemy on the map
+checkStacking(2)
+
+# place two treasure spots
+while x < 2:
+    checkStacking(3)
+    x += 1
+
+print(gameBoard)
 
 # ----------------------------------------- SET UP THE FUNCTIONS -----------------------------------------
 # create the def() functions for the program here
@@ -72,7 +92,7 @@ def movePlayer():
 #check if the player has reached the exit
 def checkExit():
     global gameOver
-    if playerX == exitX and playerY == exitY:
+    if gameBoard[playerX][playerY] == 5:
         print('You have reached the exit')
         gameOver = True
 
