4f049b62601ab14d4b3465f67a7f855a1c1649e7
OllieBL <oliver.black4@education.nsw.gov.au>
2024-08-13 17:38:47 +1000

    cleaning
    I added the data cleaning function to the program. It removes several columns where the information is either redundant or useless. The code worked fully as intended

--- my files/Data analysis work/ImplementingData.py
+++ my files/Data analysis work/ImplementingData.py
@@ -4,4 +4,12 @@ import pandas as pd
 # imports data
 olympics_df = pd.read_csv('my files/Data analysis work/athlete_events.csv')
 
-olympics_df
+# data cleaning function
+def clean():
+    global olympics_df
+    olympics_df.drop(columns=['Team', 'Year', 'Season', 'Sport'], inplace=True)
+
+# 
+
+clean()
+
