e850f5beff192961c092b156e9768266394e3581
OllieBL <oliver.black4@education.nsw.gov.au>
2024-08-14 12:05:57 +1000

    unfinished thing
    attempt to create mean, does not work because it can't access the specific rows correctly

--- my files/Data analysis work/ImplementingData.py
+++ my files/Data analysis work/ImplementingData.py
@@ -1,15 +1,32 @@
 # Import modules
 import pandas as pd
 
+y = []
+
 # imports data
 olympics_df = pd.read_csv('my files/Data analysis work/athlete_events.csv')
+original_olympics_df = olympics_df
 
 # data cleaning function
 def clean():
     global olympics_df
     olympics_df.drop(columns=['Team', 'Year', 'Season', 'Sport'], inplace=True)
 
-# 
+# data analysis function
+def analyse(request):
+    global olympics_df
+    olympics_df = olympics_df[olympics_df[request['category']] == request['value']]
+    if request['option'] == 'mean':
+        for x in olympics_df:
+            y.append(olympics_df[request['numRef']])
+        
+        
+    
 
-clean()
-
+request = {
+    'option' : 'mean',
+    'category' : 'NOC',
+    'value' : 'AUT',
+    'numRef' : 'Age'
+}
+analyse(request)
