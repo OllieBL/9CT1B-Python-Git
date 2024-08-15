83e6e1190fe71f0a08f65574be8e1e8af3c040a0
OllieBL <oliver.black4@education.nsw.gov.au>
2024-08-15 10:10:10 +1000

    tried to get the mean to work
    mean failed to work because the I couldn't index it. I've got to figure out how to index it to get it to work

--- my files/Data analysis work/ImplementingData.py
+++ my files/Data analysis work/ImplementingData.py
@@ -1,24 +1,27 @@
 # Import modules
 import pandas as pd
 
-y = []
+meanList = []
 
 # imports data
 olympics_df = pd.read_csv('my files/Data analysis work/athlete_events.csv')
 original_olympics_df = olympics_df
-
+olympics_df = pd.DataFrame(index=[range(len(olympics_df))])
 # data cleaning function
 def clean():
     global olympics_df
-    olympics_df.drop(columns=['Team', 'Year', 'Season', 'Sport'], inplace=True)
+    olympics_df.drop(columns=['Team', 'Year', 'Season'], inplace=True)
 
 # data analysis function
 def analyse(request):
     global olympics_df
     olympics_df = olympics_df[olympics_df[request['category']] == request['value']]
+    olympics_df.dropna(subset=[request['numRef']])
     if request['option'] == 'mean':
-        for x in olympics_df:
-            y.append(olympics_df[request['numRef']])
+        for i in range(len(olympics_df)):
+            meanList.append(olympics_df.iloc[3][i])
+        mean = sum(meanList) / len(meanList)
+        return mean
         
         
     
@@ -29,4 +32,4 @@ request = {
     'value' : 'AUT',
     'numRef' : 'Age'
 }
-analyse(request)
+print(analyse(request))
