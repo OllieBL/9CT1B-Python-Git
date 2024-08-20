f7fd3caead11cc5be490960eabe7c9ad89186b9f
OllieBL <oliver.black4@education.nsw.gov.au>
2024-08-20 17:56:17 +1000

    fixed issue and added median
    I fixed the issue with the mean by using the implemented pandas method as opposed to iterating over it. I also implemented the median using the corresponding method. I need to find further things about the dataset that is important, as well as making graphs

--- my files/Data analysis work/ImplementingData.py
+++ my files/Data analysis work/ImplementingData.py
@@ -5,16 +5,13 @@ import pandas as pd
 # imports data
 olympics_df = pd.read_csv('my files/Data analysis work/athlete_events.csv')
 original_olympics_df = olympics_df
-olympics_df = pd.DataFrame(olympics_df, index=['ID', 'Sex', 'Age', 'Height', 'Weight', 'Team', 'NOC', 'Games', 'Year', 'Season', 'City', 'Sport', 'Event', 'Medal'])
 # data cleaning function
 def clean():
     global olympics_df
-    olympics_df.drop(columns=['Team', 'Year', 'Season'], inplace=True)
+    olympics_df.drop(columns=['Team', 'Games'], inplace=True)
 
 # data analysis function
-
 def analyse(request):
-    meanList = []
     category = request['category']
     value = request['value']
     option = request['option']
@@ -23,10 +20,11 @@ def analyse(request):
     olympics_df = olympics_df[olympics_df[category] == value]
     olympics_df.dropna(subset=[request['numRef']])
     if option == 'mean':
-        for index, row in olympics_df.iterrows():
-           meanList.append(float(row[numRef]))
-        mean = sum(meanList) / len(meanList)
+        mean = olympics_df[numRef].mean()
         return mean
+    elif option == 'median':
+        median = olympics_df[numRef].median()
+        return median
      
         
     
@@ -35,6 +33,13 @@ request = {
     'option' : 'mean',
     'category' : 'NOC',
     'value' : 'AUT',
-    'numRef' : 'Age'
+    'numRef' : 'Height'
+}
+print(analyse(request))
+request = {
+    'option' : 'mean',
+    'category' : 'Year',
+    'value' : 1992,
+    'numRef' : 'Height'
 }
 print(analyse(request))
