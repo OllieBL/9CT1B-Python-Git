98400bc19a6c8fba41465a4c42076c31fc9af09d
OllieBL <oliver.black4@education.nsw.gov.au>
2024-08-23 14:10:25 +1000

    making UI
    final stretch

--- my files/Data analysis work/ImplementingData.py
+++ my files/Data analysis work/ImplementingData.py
@@ -50,26 +50,62 @@ def analyse(request):
     return output
      
 def UI():
+    global olympics_df
     while True:
-        if input('calculations? True or False ') == True:
+        check = input('calculations? True or False ')
+        if check == True:
             option = input('median, mean, mode, max or minimum? ')
-        if type(filter) != bool and type(option) == str:
-            print('please enter a valid option')
+            if type(check) != bool and type(option) == str:
+                print('please enter a valid option')
         else:
             break
     while True:
-        filter = input('Would you like to filter? True or False ')
+        filter = bool(input('Would you like to filter? True or False '))
         if type(filter) != bool:
             print('please enter a valid option')
         else:
             break
     while True:
         if filter == True:
-            filter = input('What column to filter? Age, Height,  ')
-            if type(filter) != bool:
+            category = input('What column to filter? Age, Sex, Sport, NOC or Event')
+            if category != 'Age' or category != 'Sport' or category != 'Event' or category != 'NOC':
                 print('please enter a valid option')
             else:
                 break
+        else:
+            break
+    while True:
+        if filter == True and type(category) == str:
+            value = input('What value do you want to sort with? ')
+            if value in olympics_df[category]:
+                print('please enter a valid option')
+            else:
+                break
+        else:
+            break
+    while True: 
+        Ref = input('what column do you want the data to display with? Sex, Age, Height, Weight, NOC, Sport or Event? ')
+        if Ref != 'Age' or Ref != 'Sport' or Ref != 'Event' or Ref != 'Sex' or Ref != 'NOC'or Ref != 'Height' or Ref != 'Weight':
+            print('please enter a valid option')
+        else:
+            break
+    while True:
+        plot = input('do you want a plot? True or False ')
+        if type(plot) != bool:
+            print('please enter a valid option')
+        else:
+            break
+    request = {
+        'category' : category,
+        'filter' : filter,
+        'value' : value,
+        'plot' : plot,
+        'Ref' : Ref
+    }
+    x = analyse(request)
+    if check == True:
+        print(x)
+        
 
      
 
