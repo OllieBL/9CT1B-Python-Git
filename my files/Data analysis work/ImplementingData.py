33148fa41839ac556d50627bba224c09ff09ff06
OllieBL <oliver.black4@education.nsw.gov.au>
2024-08-23 14:44:35 +1000

    finished code

--- my files/Data analysis work/ImplementingData.py
+++ my files/Data analysis work/ImplementingData.py
@@ -16,7 +16,8 @@ def analyse(request):
     value = request['value']
     option = request['option']
     Ref = request['Ref']
-    plot = request['plot']
+    plot = bool(request['plot'])
+    output = 0
     global olympics_df
     counter = []
 
@@ -31,6 +32,7 @@ def analyse(request):
         median = olympics_df[Ref].median()
         output = median
     if plot == True:
+        checker = olympics_df
         if type(olympics_df[Ref].values[0]) == int or type(olympics_df[Ref].values[0]) == float:
             checker =  pd.cut(olympics_df[Ref], 10, labels=False)
             print(checker)
@@ -41,7 +43,7 @@ def analyse(request):
                 counter.append(int(c['Medal'].value_counts()['Gold']))
             else:
                 counter.append(0)
-        dict = {'counter' : counter, 'sort' : checker.Height.unique()}
+        dict = {'counter' : counter, 'sort' : pd.unique(checker[Ref])}
         df = pd.DataFrame(dict)
         df.sort_values(by=['counter'], ascending=False, inplace=True)
         df = df.head(10)
@@ -50,57 +52,66 @@ def analyse(request):
     return output
      
 def UI():
+    check = ''
+    option = ''
+    filter = ''
+    category = ''
+    value = ''
+    
     global olympics_df
     while True:
         check = input('calculations? True or False ')
-        if check == True:
-            option = input('median, mean, mode, max or minimum? ')
-            if type(check) != bool and type(option) == str:
+        if check == 'True':
+            option = input('median or mean? ')
+            if option == 'median' or option == 'mean' :
+                break
+            else:
                 print('please enter a valid option')
         else:
             break
     while True:
-        filter = bool(input('Would you like to filter? True or False '))
-        if type(filter) != bool:
+        filter = input('Would you like to filter? True or False ')
+        if filter == 'True' or filter == 'False':
+            break
+        else:
             print('please enter a valid option')
-        else:
-            break
     while True:
-        if filter == True:
+        if filter == 'True':
             category = input('What column to filter? Age, Sex, Sport, NOC or Event')
-            if category != 'Age' or category != 'Sport' or category != 'Event' or category != 'NOC':
-                print('please enter a valid option')
-            else:
+            if category == 'Age' or category == 'Sport' or category == 'Event' or category == 'NOC':
                 break
+            else:
+                print('please enter a valid option')
         else:
             break
     while True:
-        if filter == True and type(category) == str:
+        if filter == 'True' and type(category) == str:
             value = input('What value do you want to sort with? ')
-            if value in olympics_df[category]:
-                print('please enter a valid option')
-            else:
+            if (olympics_df == value).any().any():
                 break
+            else:
+                print('please enter a valid option')
         else:
             break
     while True: 
         Ref = input('what column do you want the data to display with? Sex, Age, Height, Weight, NOC, Sport or Event? ')
-        if Ref != 'Age' or Ref != 'Sport' or Ref != 'Event' or Ref != 'Sex' or Ref != 'NOC'or Ref != 'Height' or Ref != 'Weight':
-            print('please enter a valid option')
-        else:
+        if Ref == 'Age' or Ref == 'Sport' or Ref == 'Event' or Ref == 'Sex' or Ref == 'NOC'or Ref == 'Height' or Ref == 'Weight':
             break
+        else:
+            print('please enter a valid option')
     while True:
         plot = input('do you want a plot? True or False ')
-        if type(plot) != bool:
-            print('please enter a valid option')
-        else:
+        if plot == 'True' or plot == 'False':
             break
+        else:
+            print('please enter a valid option')
     request = {
+        'option' : option,
         'category' : category,
-        'filter' : filter,
         'value' : value,
         'plot' : plot,
-        'Ref' : Ref
+        'Ref' : Ref,
+        'sort' : filter
     }
     x = analyse(request)
     if check == True:
